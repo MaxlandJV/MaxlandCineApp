56b36d5966f3964f8915a128f6d1d57c89968086
Jordi Villaró <jvilarof@gmail.com>
2022-01-29 11:29:11 +0100

    Initial Commit

---
+++ TestSwiftApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestSwiftApp
+//
+//  Created by Jordi Villaró on 29/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestSwiftApp/TestSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftAppApp.swift
+//  TestSwiftApp
+//
+//  Created by Jordi Villaró on 29/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
