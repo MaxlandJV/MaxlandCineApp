5c894d6a38fe973a409030050355d03e3540646f
Jordi Villaró <jvilarof@gmail.com>
2022-03-02 15:26:24 +0100

    Segunda intentona de fondo degradado.

--- TestSwiftApp/MovieListView.swift
+++ TestSwiftApp/MovieListView.swift
@@ -11,36 +11,41 @@ struct MovieListView: View {
     @StateObject var movies = MoviesViewModel()
     @State var isPresented: Bool = false
     
+    init() {
+        UITableView.appearance().backgroundColor = UIColor.clear
+        UITableViewCell.appearance().backgroundColor = .clear
+    }
+    
     var body: some View {
-        ZStack {
+        NavigationView {
             LinearGradient(colors: [Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))], startPoint: .topLeading, endPoint: .bottomTrailing)
-            NavigationView {
-                List(movies.movieList) { movieItem in
-                    NavigationLink(destination: MovieView(movie: movieItem, movies: movies, update: true)) {
-                        MovieListRowView(movie: movieItem)
-                    }
-                    .swipeActions(edge: .leading) {
-                        Button {
-                            movies.deleteMovie(id: movieItem.id)
-                        } label: {
-                            Label("Eliminar", systemImage: "trash.fill")
+                .ignoresSafeArea()
+                .overlay(
+                    List(movies.movieList) { movieItem in
+                        NavigationLink(destination: MovieView(movie: movieItem, movies: movies, update: true)) {
+                            MovieListRowView(movie: movieItem)
                         }
-                        .tint(.red)
+                        .swipeActions(edge: .leading) {
+                            Button {
+                                movies.deleteMovie(id: movieItem.id)
+                            } label: {
+                                Label("Eliminar", systemImage: "trash.fill")
+                            }
+                            .tint(.red)
+                        }
                     }
-                }
-                .listStyle(PlainListStyle())
-                .navigationTitle(Text("Películas"))
-                .navigationBarItems(trailing: Button {
-                    isPresented.toggle()
-                } label: {
-                    Image(systemName: "plus.circle")
-                })
-                .sheet(isPresented: $isPresented) {
-                    MovieView(movies: movies, update: false)
-                }
-            }
+                        .listStyle(PlainListStyle())
+                        .navigationTitle(Text("Películas"))
+                        .navigationBarItems(trailing: Button {
+                            isPresented.toggle()
+                        } label: {
+                            Image(systemName: "plus.circle")
+                        })
+                        .sheet(isPresented: $isPresented) {
+                            MovieView(movies: movies, update: false)
+                        }
+                )
         }
-        .ignoresSafeArea()
     }
 }
 
