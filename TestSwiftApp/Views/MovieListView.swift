4981883b9f602034ee9d3124f878603d7bf56bc6
Jordi Villaró <jvilarof@gmail.com>
2022-04-28 19:55:05 +0200

    Implementación del nuevo contro searchable.

--- TestSwiftApp/Views/MovieListView.swift
+++ TestSwiftApp/Views/MovieListView.swift
@@ -40,29 +40,6 @@ struct MovieListView: View {
                         }
                         else {
                             VStack {
-                                HStack {
-                                    Image(systemName: "magnifyingglass")
-                                        .foregroundColor(
-                                            searchMovie.isEmpty ? Color(UIColor.systemGray2) : Color.primary
-                                        )
-                                    TextField("Buscar una película...", text: $searchMovie)
-                                        .disableAutocorrection(true)
-                                        .overlay(
-                                            Image(systemName: "xmark.circle.fill")
-                                                .opacity(searchMovie.isEmpty ? 0.0 : 1.0)
-                                                .onTapGesture {
-                                                    searchMovie = ""
-                                                }, alignment: .trailing
-                                        )
-                                }
-                                .font(.headline)
-                                .padding(8)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 25)
-                                        .fill(Color(UIColor.systemGray3))
-                                )
-                                .padding()
-                                
                                 List(searchResults) { movie in
                                     NavigationLink(destination: MovieView(movie: movie, update: true)) {
                                         MovieListRowView(movieName: movie.movieName, showDate: movie.showDate, sinopsis: movie.sinopsis, score: movie.score)
@@ -82,20 +59,20 @@ struct MovieListView: View {
                             }
                         }
                     }
-                        .navigationTitle(Text("Películas"))
-                        .navigationBarItems(leading: NavigationLink(destination: MovieSetupView()) {
-                            Image(systemName: "gearshape")
-                                .foregroundColor(.black)
-                        })
-                        .navigationBarItems(trailing: Button {
-                            isPresented.toggle()
-                        } label: {
-                            Image(systemName: "plus.circle")
-                                .foregroundColor(.black)
-                        })
-                        .sheet(isPresented: $isPresented) {
-                            MovieView()
-                        }
+                    .navigationTitle(Text("Películas"))
+                    .navigationBarItems(leading: NavigationLink(destination: MovieSetupView()) {
+                        Image(systemName: "gearshape")
+                            .foregroundColor(.black)
+                    })
+                    .navigationBarItems(trailing: Button {
+                        isPresented.toggle()
+                    } label: {
+                        Image(systemName: "plus.circle")
+                            .foregroundColor(.black)
+                    })
+                    .sheet(isPresented: $isPresented) {
+                        MovieView()
+                    }
                 )
         }
     }
