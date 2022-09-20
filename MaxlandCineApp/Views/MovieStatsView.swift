7d53ce601e82cb6d52ce58481154d8d42e726042
Jordi Villaró <jvilarof@gmail.com>
2022-09-20 11:45:01 +0200

    Añadir la posibilidad de generar varias gráficas.

--- MaxlandCineApp/Views/MovieStatsView.swift
+++ MaxlandCineApp/Views/MovieStatsView.swift
@@ -16,72 +16,14 @@ struct MovieStatsView: View {
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack {
-                VStack(spacing: 20) {
-                    HStack {
-                        Text("stats-movie-number")
-                            .font(.headline)
-                        Spacer()
-                        Text("\(movieViewModel.getNumberMovies())")
-                            .font(.title3)
-                            .fontWeight(.bold)
-                    }
-                    
-                    HStack {
-                        Capsule()
-                            .fill(Color.blue)
-                            .frame(width: 20, height: 8)
-                        Text("stats-movie-score")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    HStack(spacing: 0) {
-                        MovieStatsGraphBarView(starsNumber: 1, movies: movieListScore[0], score: score[0])
-                        MovieStatsGraphBarView(starsNumber: 2, movies: movieListScore[1], score: score[1])
-                        MovieStatsGraphBarView(starsNumber: 3, movies: movieListScore[2], score: score[2])
-                        MovieStatsGraphBarView(starsNumber: 4, movies: movieListScore[3], score: score[3])
-                        MovieStatsGraphBarView(starsNumber: 5, movies: movieListScore[4], score: score[4])
-                    }
-                    .padding(.top, 20)
-                    .frame(height: 280)
-                }
-                .padding()
-                .background(Color(UIColor.systemGray6))
-                .cornerRadius(18)
+                MovieStatsViewGraph(numberOfMovies: movieViewModel.getNumberMovies(), movieListScore: movieViewModel.getNumberMoviesByScore())
+                MovieStatsViewGraph(numberOfMovies: movieViewModel.getNumberMovies(), movieListScore: movieViewModel.getNumberMoviesByScore())
             }
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(UIColor.systemGray5))
         .navigationBarTitle("stats-title")
-        .onAppear {
-            movieListScore = movieViewModel.getNumberMoviesByScore()
-            withAnimation(.linear(duration: 0.5)) {
-                score[0] = getBarHeight(valor: CGFloat(movieListScore[0]))
-                score[1] = getBarHeight(valor: CGFloat(movieListScore[1]))
-                score[2] = getBarHeight(valor: CGFloat(movieListScore[2]))
-                score[3] = getBarHeight(valor: CGFloat(movieListScore[3]))
-                score[4] = getBarHeight(valor: CGFloat(movieListScore[4]))
-            }
-        }
-    }
-    
-    // MARK: Obtener el valor máximo
-    func getMax() -> CGFloat {
-        let max = movieListScore.max { valor1, valor2 in
-            return valor2 > valor1
-        } ?? 0
-        
-        return CGFloat(max)
-    }
-    
-    // MArK: Obtener el tamaño de cada barra del gráfico
-    func getBarHeight(valor: CGFloat) -> CGFloat {
-        let max = getMax()
-        let height = max > 0 ? (valor * 180) / max : 0
-        
-        return height
     }
 }
 
