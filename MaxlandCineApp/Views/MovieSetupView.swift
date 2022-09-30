393703d300a7ddc295e72533aa9033789b2b2484
Jordi Villaró <jvilarof@gmail.com>
2022-09-30 12:07:11 +0200

    Publicada la versión 1.1.2 y empezada la 1.2.0 compatible con iOS 16

--- MaxlandCineApp/Views/MovieSetupView.swift
+++ MaxlandCineApp/Views/MovieSetupView.swift
@@ -13,7 +13,7 @@ struct MovieSetupView: View {
     
     var body: some View {
         Form {
-            Section(header: Text("setup-about")) {
+            Section(header: Text("setup-about") + Text(" - 1.2.0")) {
                 VStack(alignment: .leading) {
                     Image("MaxlandWorld")
                         .resizable()
