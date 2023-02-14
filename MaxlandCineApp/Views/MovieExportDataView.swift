6892f6cacfcc9d4cdfe6e2ada74d019841ccffce
Jordi Villaró <jvilarof@gmail.com>
2023-02-14 12:07:07 +0100

    Terminada la exportación de datos.

--- MaxlandCineApp/Views/MovieExportDataView.swift
+++ MaxlandCineApp/Views/MovieExportDataView.swift
@@ -8,34 +8,41 @@
 import SwiftUI
 
 struct MovieExportDataView: View {
+    @Environment(\.dismiss) var dismiss
     
     @EnvironmentObject var movieViewModel: MovieViewModel
     @State private var exportFile: Bool = false
+    @State var showingAlert = false
     
     var body: some View {
         VStack {
-            Text("Mediante esta opción se pueden exportar todos los datos contenidos en la aplicación a un archivo de texto en formato JSON")
+            Text("movie-export-detail")
                 .font(.headline)
             Image(systemName: "doc.text")
                 .resizable()
                 .frame(width: 150, height: 190)
-                .padding(.top, 100)
+                .padding(.top, 50)
+                .foregroundColor(.accentColor)
+            Text("mxlcdata.json")
+                .font(.headline)
             Button {
                 exportFile.toggle()
             } label: {
-                Text("movie-export-Data")
+                Text("movie-export-data")
             }
-            .padding(.top, 20)
+            .padding(.top, 50)
             .buttonStyle(.borderedProminent)
             .font(.title2)
             .fileExporter(isPresented: $exportFile, document: JsonFile(json: movieViewModel.getJSONData() ?? ""), contentType: .json, defaultFilename: "mxlcdata") { result in
                 switch result {
                 case .success(_):
-                    print("Exportado correctamente")
+                    showingAlert.toggle()
+                    dismiss()
                 case .failure(let failure):
                     fatalError(failure.localizedDescription)
                 }
             }
+            .alert("movie-export-alert", isPresented: $showingAlert) {}
             Spacer()
         }
         .padding()
