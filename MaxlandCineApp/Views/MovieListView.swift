b35478c2beda0cb63b85153e3d95945d6bcc8b0d
Jordi Villaró <jvilarof@gmail.com>
2023-03-17 12:59:01 +0100

    Merge amb la rama NewLayout

--- MaxlandCineApp/Views/MovieListView.swift
+++ MaxlandCineApp/Views/MovieListView.swift
@@ -75,69 +75,6 @@ struct MovieListView: View {
             .background(LinearGradient(colors: [Color("TopColorGradient"), Color("BottomColorGradient")], startPoint: .topLeading, endPoint: .bottomTrailing))
         }
         .navigationViewStyle(StackNavigationViewStyle())
-
-//        NavigationView {
-//           LinearGradient(colors: [Color("TopColorGradient"), Color("BottomColorGradient")], startPoint: .topLeading, endPoint: .bottomTrailing)
-//               .ignoresSafeArea()
-//               .overlay (
-//                    ZStack {
-//                        if movieViewModel.movieList.isEmpty {
-//                            MovieEmptyView()
-//                                .transition(AnyTransition.opacity.animation(.easeIn))
-//                        }
-//                        else {
-//                            VStack {
-//                                List(searchResults) { movie in
-//                                    NavigationLink(destination: MovieView(movie: movie, update: true)) {
-//                                        MovieListRowView(movieName: movie.movieName, showDate: movie.showDate, sinopsis: movie.sinopsis, score: movie.score, isSerie: movie.isSerie)
-//                                    }
-//                                    .swipeActions(edge: .leading) {
-//                                        Button {
-//                                            movieViewModel.deleteMovie(movie: movie)
-//                                        } label: {
-//                                            Label("Eliminar", systemImage: "trash.fill")
-//                                        }
-//                                        .tint(.red)
-//                                    }
-//                                    .listRowBackground(Color.white.opacity(0))
-//                                    .listRowSeparator(.hidden)
-//                                    .listRowInsets(EdgeInsets(top: 0, leading: 15, bottom: 5, trailing: 15))
-//                                }
-//                                .scrollContentBackground(.hidden)
-//                                .listStyle(.plain)
-//                                .searchable(text: $searchMovie, prompt: "navigation-list-search")
-//                            }
-//                        }
-//                    }
-//                    //.background(Color.green)
-//                    .navigationTitle(Text("navigation-list-title"))
-//                    .toolbar(content: {
-//                        ToolbarItem(placement: .navigationBarLeading) {
-//                            NavigationLink(destination: MovieSetupView()) {
-//                                Image(systemName: "gearshape").foregroundColor(.black)
-//                            }
-//                        }
-//                        ToolbarItem(placement: .navigationBarLeading) {
-//                            if (movieViewModel.movieList.count > 0) {
-//                                NavigationLink(destination: MovieStatsView()) {
-//                                    Image(systemName: "chart.bar.xaxis").foregroundColor(.black)
-//                                }
-//                            }
-//                        }
-//                        ToolbarItem(placement: .navigationBarTrailing) {
-//                            Button {
-//                                isPresented.toggle()
-//                            } label: {
-//                                Image(systemName: "plus.circle").foregroundColor(.black)
-//                            }
-//                        }
-//                    })
-//                    .sheet(isPresented: $isPresented) {
-//                        MovieView()
-//                    }
-//                )
-//        }
-//        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
