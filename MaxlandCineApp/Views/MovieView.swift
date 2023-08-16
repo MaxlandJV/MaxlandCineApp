e3f7d192ca831313a24edbe744c25633cfc805de
Jordi Villaró <jvilarof@gmail.com>
2023-08-16 10:32:34 +0200

    Nuevas pruebas adicionales.

--- MaxlandCineApp/Views/MovieView.swift
+++ MaxlandCineApp/Views/MovieView.swift
@@ -34,163 +34,168 @@ struct MovieView: View {
     var update: Bool = false
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 0) {
-                if let selectedImage {
-                    Image(uiImage: selectedImage)
+        ZStack {
+            ScrollView {
+
+ //               if let selectedImage {
+                    Image("Tron")
                         .resizable()
                         .scaledToFill()
                         .clipped()
                         .padding(.vertical)
-                }
+                        .opacity(0.5)
+//                }
                 
-                if !update {
-                    TextField("movie-name", text: $movieName)
-                        .focused($movieNameInfocus)
-                        .padding()
+                VStack(spacing: 0) {
+                    if !update {
+                        TextField("movie-name", text: $movieName)
+                            .focused($movieNameInfocus)
+                            .padding()
+                            .cornerRadius(10)
+                            .disableAutocorrection(true)
+                            .padding(.bottom, 10)
+                    }
+                    
+                    Divider()
+                    
+                    VStack {
+                        DatePicker("movie-date",selection: $showDate, displayedComponents: .date)
+                            .padding(.top, 8)
+                        
+                        Divider()
+                        
+                        Toggle(isSerie ? "movie-type-1" : "movie-type-2", isOn: $isSerie)
+                            .toggleStyle(CheckboxStyle())
+                        
+                        Divider()
+                    }
+                    
+                    HStack {
+                        Text("movie-score")
+                        Spacer()
+                        ForEach(1...5, id: \.self) { number in
+                            Image(systemName: "star.fill")
+                                .foregroundColor(number > score ? Color("StarNoActive") : .orange)
+                                .onTapGesture {
+                                    score = Int16(number)
+                                }
+                        }
+                    }
+                    .padding(.vertical, 10)
+                    
+                    Divider()
+                    
+                    HStack {
+                        Text("movie-review")
+                            .font(.headline)
+                            .padding(.top, 10)
+                        Spacer()
+                    }
+                    TextEditor(text: $sinopsis)
                         .cornerRadius(10)
-                        .disableAutocorrection(true)
-                        .padding(.bottom, 10)
-                }
-                
-                Divider()
-                
-                VStack {
-                    DatePicker("movie-date",selection: $showDate, displayedComponents: .date)
-                        .padding(.top, 8)
+                        .scrollContentBackground(.hidden)
+                        .background(.clear)
+                        .frame(height: 300)
                     
                     Divider()
-                    
-                    Toggle(isSerie ? "movie-type-1" : "movie-type-2", isOn: $isSerie)
-                        .toggleStyle(CheckboxStyle())
-                    
-                    Divider()
-                }
-                
-                HStack {
-                    Text("movie-score")
-                    Spacer()
-                    ForEach(1...5, id: \.self) { number in
-                        Image(systemName: "star.fill")
-                            .foregroundColor(number > score ? Color("StarNoActive") : .orange)
-                            .onTapGesture {
-                                score = Int16(number)
-                            }
+                                  
+                    if !update {
+                        PhotosPicker(selection: $selectedItem, matching: .images) {
+                            Text("movie-upload-photo")
+                        }
+                        .padding()
                     }
                 }
-                .padding(.vertical, 10)
-                
-                Divider()
-                
-                HStack {
-                    Text("movie-review")
-                        .font(.headline)
-                        .padding(.top, 10)
-                    Spacer()
-                }
-                TextEditor(text: $sinopsis)
-                    .cornerRadius(10)
-                    .scrollContentBackground(.hidden)
-                    .background(.clear)
-                    .frame(height: 300)
-                
-                Divider()
-                              
-                if !update {
-                    PhotosPicker(selection: $selectedItem, matching: .images) {
-                        Text("movie-upload-photo")
-                    }
-                    .padding()
-                }
-            }
-            .padding()
-            .navigationBarTitle(movie?.movieName ?? NSLocalizedString("movie-new", comment: ""), displayMode: .inline)
-            .toolbar {
-                if update {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button(action: updateMovie, label: {
-                            Text("movie-button-update")
-                        })
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Menu {
-                            Button {
-                                showingPhotos.toggle()
-                            } label: {
-                                Label("movie-upload-photo", systemImage: "photo")
-                            }
+                .padding()
+                .navigationBarTitle(movie?.movieName ?? NSLocalizedString("movie-new", comment: ""), displayMode: .inline)
+                .toolbar {
+                    if update {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button(action: updateMovie, label: {
+                                Text("movie-button-update")
+                            })
+                        }
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Menu {
+                                Button {
+                                    showingPhotos.toggle()
+                                } label: {
+                                    Label("movie-upload-photo", systemImage: "photo")
+                                }
 
-                            ShareLink(item: movieName + ": " + sinopsis)
-                            
+                                ShareLink(item: movieName + ": " + sinopsis)
+                                
+                                Button {
+                                    showingConfirmation.toggle()
+                                } label: {
+                                    Label("movie-confirm-delete", systemImage: "trash")
+                                }
+                            }
+                            label: {
+                                Image(systemName: "ellipsis.circle")
+                            }
+                            .confirmationDialog("", isPresented: $showingConfirmation) {
+                                Button("movie-confirm-delete", role: .destructive) {
+                                    movieViewModel.deleteMovie(movie: movie!)
+                                    dismiss()
+                                }
+                                Button("movie-button-cancel", role: .cancel) { }
+                            }
+                            .photosPicker(isPresented: $showingPhotos, selection: $selectedItem, matching: .images)
+                        }
+                    }
+                    else {
+                        ToolbarItem(placement: .navigationBarLeading) {
                             Button {
-                                showingConfirmation.toggle()
-                            } label: {
-                                Label("movie-confirm-delete", systemImage: "trash")
-                            }
-                        }
-                        label: {
-                            Image(systemName: "ellipsis.circle")
-                        }
-                        .confirmationDialog("", isPresented: $showingConfirmation) {
-                            Button("movie-confirm-delete", role: .destructive) {
-                                movieViewModel.deleteMovie(movie: movie!)
                                 dismiss()
+                            } label: {
+                                Text("movie-button-cancel")
                             }
-                            Button("movie-button-cancel", role: .cancel) { }
                         }
-                        .photosPicker(isPresented: $showingPhotos, selection: $selectedItem, matching: .images)
-                    }
-                }
-                else {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button {
-                            dismiss()
-                        } label: {
-                            Text("movie-button-cancel")
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button(action: newMovie, label: {
+                                Text("movie-button-save")
+                            })
+                            .alert("movie-error-01", isPresented: $showingAlert) {}
                         }
                     }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button(action: newMovie, label: {
-                            Text("movie-button-save")
-                        })
-                        .alert("movie-error-01", isPresented: $showingAlert) {}
-                    }
                 }
-            }
-            .onChange(of: selectedItem) { newItem in
-                Task {
-                    if let data = try? await newItem?.loadTransferable(type: Data.self) {
-                        selectedPhotoData = data
-                        if let selectedPhotoData, let image = UIImage(data: selectedPhotoData) {
-                            selectedImage = ImageManagement.resizeImage(image: image, targetSize: CGSize(width: 1024, height: 768))
-                            if let image = selectedImage {
-                                photoDataCompressed = image.jpegData(compressionQuality: 0.8)
+                .onChange(of: selectedItem) { newItem in
+                    Task {
+                        if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                            selectedPhotoData = data
+                            if let selectedPhotoData, let image = UIImage(data: selectedPhotoData) {
+                                selectedImage = ImageManagement.resizeImage(image: image, targetSize: CGSize(width: 1024, height: 768))
+                                if let image = selectedImage {
+                                    photoDataCompressed = image.jpegData(compressionQuality: 0.8)
+                                }
                             }
                         }
                     }
                 }
-            }
-            .onAppear {
-                if let updatedMovie = movie {
-                    movieName = updatedMovie.movieName ?? ""
-                    showDate = updatedMovie.showDate ?? Date()
-                    sinopsis = updatedMovie.sinopsis ?? ""
-                    score = updatedMovie.score
-                    isSerie = updatedMovie.isSerie
-                    if let imageData = updatedMovie.caratula {
-                        selectedImage = UIImage(data: imageData)
-                        photoDataCompressed = imageData
-                    }
-                } else {
-                    // Poner el foco en el campo del nombre de la película
-                    // Si se cambia el valor de "movieNameInFocus" no funciona
-                    // Hay que hacerlo desde algún disparador como un botón
-                    // o en este caso desde un evento asíncrono
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                        movieNameInfocus = true
+                .onAppear {
+                    if let updatedMovie = movie {
+                        movieName = updatedMovie.movieName ?? ""
+                        showDate = updatedMovie.showDate ?? Date()
+                        sinopsis = updatedMovie.sinopsis ?? ""
+                        score = updatedMovie.score
+                        isSerie = updatedMovie.isSerie
+                        if let imageData = updatedMovie.caratula {
+                            selectedImage = UIImage(data: imageData)
+                            photoDataCompressed = imageData
+                        }
+                    } else {
+                        // Poner el foco en el campo del nombre de la película
+                        // Si se cambia el valor de "movieNameInFocus" no funciona
+                        // Hay que hacerlo desde algún disparador como un botón
+                        // o en este caso desde un evento asíncrono
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            movieNameInfocus = true
+                        }
                     }
                 }
             }
+            .ignoresSafeArea()
         }
     }
     
